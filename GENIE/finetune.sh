48973613ba6ef9a7345f2f05192726855ff0e7c7
SpaceHunter0413 <xz479@cam.ac.uk>
2023-11-17 12:34:49 +0000

    >??>

--- GENIE/finetune.sh
+++ GENIE/finetune.sh
@@ -1,11 +1,5 @@
-OUT_DIR = "/Your/output/path"
-DATA_PATH = "/Your/data/path"
-DATA_NAME = "xsum_data"
-PRETRAIN_CKPT_PATH = "/Your/pretrain_ckpt/path"
-
-
-python -u -m torch.distributed.launch --nproc_per_node=4 --master_port=9421 \
-Genie_Finetune.py
+python -u -m torch.distributed.launch --nproc_per_node=1 --master_port=9421 \
+Genie_Finetune.py \
 --checkpoint_path="/save/delibot" \
 --model_channels 128 --in_channel 128 --out_channel 128 --vocab_size 30522 \
 --config_name="bert-base-uncased" --token_emb_type="random" --model_arch="s2s_CAT" \
